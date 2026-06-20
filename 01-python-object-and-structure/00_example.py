45784f457de7a99ffac6654f0002ce0195f59a11
Hemant Mandge <mandge.hemant@gmail.com>
2026-06-20 16:22:59 +0530

    test files

--- 01-python-object-and-structure/00_example.py
+++ 01-python-object-and-structure/00_example.py
@@ -1,1 +1,14 @@
-print("Hi Hemant")
+import string
+
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    dummy_text = str1.replace(' ','')
+    new_set = set(dummy_text.lower())
+    fnl_check = ''.join(sorted(new_set))
+    print(new_set)
+    print(fnl_check)
+    if fnl_check == alphabet:
+        return True
+    else:
+        return False
+    
+print(ispangram("The quick brown fox jumps over the lazy dog"))
